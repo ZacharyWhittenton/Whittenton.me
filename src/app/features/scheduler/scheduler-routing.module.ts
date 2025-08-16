92cbd2c870cb6ed7e7b7a534c35f552e77bce1f2
Zachary K Whittenton <zwhitty11@gmail.com>
2025-08-15 22:39:21 -0500

    Build: Started Scheduler

--- src/app/features/scheduler/scheduler-routing.module.ts
+++ src/app/features/scheduler/scheduler-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../../core/guards/auth.guard';
 import { SchedulerComponent } from './scheduler.component';
 
-const routes: Routes = [{ path: '', component: SchedulerComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    component: SchedulerComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SchedulerRoutingModule { }
+export class SchedulerRoutingModule {}
--- src/app/features/scheduler/scheduler.module.ts
+++ src/app/features/scheduler/scheduler.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { SchedulerRoutingModule } from './scheduler-routing.module';
 import { SchedulerComponent } from './scheduler.component';
-
+import { MeetingFormComponent } from './components/meeting-form/meeting-form.component';
+import { MeetingListComponent } from './components/meeting-list/meeting-list.component';
 
 @NgModule({
   declarations: [
-    SchedulerComponent
+    SchedulerComponent,
+    MeetingFormComponent,
+    MeetingListComponent,
   ],
   imports: [
     CommonModule,
-    SchedulerRoutingModule
-  ]
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    SchedulerRoutingModule,
+  ],
 })
-export class SchedulerModule { }
+export class SchedulerModule {}
